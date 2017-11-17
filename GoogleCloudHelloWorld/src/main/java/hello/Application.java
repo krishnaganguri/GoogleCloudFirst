bed6d15aba31b2f6f80066d936659d8391c3a04d
Krishna kanth <krishnakanth876@gmail.com>
2017-11-16 22:09:08 -0500

    Add files via upload

---
+++ GoogleCloudHelloWorld/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+    	System.getProperties().put( "server.port", 85 );
+        SpringApplication.run(Application.class, args);
+    }
+
+}
